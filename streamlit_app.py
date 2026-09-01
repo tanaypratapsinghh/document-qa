b7383a64e102b1b7d88b8a74c307ec59c286fe19
tanaypratapsinghh <tsingh13@syr.edu>
2026-09-01 15:09:29 +0000

    default lab 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 
-lab1 = st.Page("Lab1.py", title="Lab 1", default=True)
-lab2 = st.Page("Lab2.py", title="Lab 2")
+lab1 = st.Page("Lab1.py", title="Lab 1")
+lab2 = st.Page("Lab2.py", title="Lab 2", default=True)
 
 pg = st.navigation([lab1, lab2])
 pg.run()
